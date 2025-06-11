dc178258be79999916bfb73ab8d8c3c41f93f9aa
Samyayan <bitanrockz@gmail.com>
2025-06-11 09:40:50 +0530

    Update earthquake_1906-2023.py

--- earthquake_1906-2023.py
+++ earthquake_1906-2023.py
@@ -32,8 +32,9 @@ def global_distribution(self):
     fig.update_traces(marker=dict(color=col))
     st.plotly_chart(fig, use_container_width=True)
 
- 
+st.set_page_config(layout="wide")
 st.title(":point_right: Global earthquake EDA 1906-2023")
+st.markdown('<style>div.block-container{margin-top:0; padding-top:2rem;}</style>', unsafe_allow_html=True)
 
 upload_file=st.sidebar.file_uploader("Upload file", type=["csv", "txt", "xlsx", "xls"])
 options=st.sidebar.radio("Pages", options=["Home", "Data statistics", "Data header", "Interactive plots", "Map view"])
