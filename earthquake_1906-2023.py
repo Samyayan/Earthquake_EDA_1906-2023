3b5bbb06fe04880743e83773bf640b553dbb92ff
Samyayan <bitanrockz@gmail.com>
2025-06-11 08:46:27 +0530

    Update earthquake_1906-2023.py

--- earthquake_1906-2023.py
+++ earthquake_1906-2023.py
@@ -23,6 +23,15 @@ def plot(dataframe):
     sc_plot.update_traces(marker=dict(color=col))
     st.plotly_chart(sc_plot, use_container_width=True)
 
+def global_distribution(self):
+    st.markdown("### :round_pushpin: Global earthquake distribution.")
+    st.text("Place your cursor to get details of the magnitude for repsective points")
+    col=st.color_picker("Select a color for plot")
+    fig=px.scatter_mapbox(df, lat="latitude", lon="longitude", hover_name="mag", zoom=1)
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_traces(marker=dict(color=col))
+    st.plotly_chart(fig, use_container_width=True)
+
  
 st.title(":point_right: Global earthquake EDA 1906-2023")
 
@@ -30,22 +39,21 @@ upload_file=st.sidebar.file_uploader("Upload file", type=["csv", "txt", "xlsx", 
 options=st.sidebar.radio("Pages", options=["Home", "Data statistics", "Data header", "Interactive plots", "Map view"])
 if upload_file:
     df=pd.read_csv(upload_file)
+try:
+    if options=="Home":
+        st.markdown("### This is a web app to explore earthquake data")
+        home(df)
+    elif options=="Data statistics":
+        stats(df)
+    elif options=="Data header":
+        header(df)
+    elif options=="Interactive plots":
+        plot(df)
+    elif options=="Map view":
+        global_distribution(df)
+        
+except: st.markdown("## Upload global eathquake data")
 
-if options=="Home":
-    home(df)
-    st.markdown("### This is a web app to explore earthquake data")
-elif options=="Data statistics":
-    stats(df)
-elif options=="Data header":
-    header(df)
-elif options=="Interactive plots":
-    plot(df)
-elif options=="Map view":
-    col=st.color_picker("Select a color for plot")
-    fig=px.scatter_mapbox(df, lat="latitude", lon="longitude", hover_name="mag", zoom=1)
-    fig.update_layout(mapbox_style="open-street-map")
-    fig.update_traces(marker=dict(color=col))
-    st.plotly_chart(fig, use_container_width=True)
     
 
 
